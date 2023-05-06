dc9321fdf10cc30b7230f3389654af6afe9031bd
powpowpow09 <netgeek@orange.fr>
2023-05-06 08:47:46 +0200

    ubuntu server config

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-at)%s9q*24jn$eylc53wkkivt%$lts)--imdmnpb3w32@)uzv0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["192.168.197.128", "localhost", "127.0.0.1"]
 
 
 # Application definition
